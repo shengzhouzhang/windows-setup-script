198154ba5a9c1a2a9b43c3a68e9125101a01f82b
Shengzhou Zhang <steven.shengzhou@gmail.com>
2014-01-07 16:01:25 +1100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 @Echo off
 
 echo Runing Boxstarter..
-start /wait http://boxstarter.org/package/url?https://raw.github.com/szha246/Chocolatey-Scripts/master/windows-initializer
+start /wait http://boxstarter.org/package/url?https://raw.github.com/szha246/windows-setup-script/master/boxstarter-script
